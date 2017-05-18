f9ea727be6623dc07a4ddfaf862c2a115b5e55e3
Andrew Sonin <sonin.cel@gmail.com>
2017-05-18 20:55:50 +0300

    Update _01_read_simple_graph.py

--- _01_read_simple_graph.py
+++ _01_read_simple_graph.py
@@ -1,7 +1,7 @@
 def read_simple_graph(vertex_number, edge_number):
     graph = [[0] * vertex_number for i in range(vertex_number)]
     for j in range(edge_number):
-        l1, l2 = list(map(int, input().split()))
+        l1, l2 = map(int, input().split())
         graph[l1][l2] = 1
         graph[l2][l1] = 1
     return graph
